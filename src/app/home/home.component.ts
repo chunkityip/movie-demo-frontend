fd300029373ef1fcd2ca36b2b8400c0b45c2bacd
Muazzum Akhtar <muazzum.akhtar@peopleshores.com>
2024-10-25 14:02:11 -0400

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,30 +3,45 @@ import { MovieShowingComponent } from '../movie-showing/movie-showing.component'
 import { MovieShowing } from '../movieshowing';
 import { CommonModule } from '@angular/common';
 import { DataDisplayComponent } from '../data-display/data-display.component';
+import { TouchSliderComponent } from '../touch-slider/touch-slider.component';
+import { HammerModule } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MovieShowingComponent, CommonModule, DataDisplayComponent],
+  imports: [MovieShowingComponent,HammerModule, CommonModule, DataDisplayComponent, TouchSliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
-  movieShowingList: MovieShowing[] = [
+  // movieShowingList: MovieShowing[] = [
+  //   {
+  //     id: 0,
+  //     name: 'Star Wars',
+  //     description: 'Once Upon a Time',
+  //     photo: `${this.baseUrl}/example-house.jpg`,
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'Clone Wars',
+  //     description: 'Once Upon a Time',
+  //     photo: `${this.baseUrl}/example-house.jpg`,
+  //   },
+  // ];
+
+  title = 'touch-slider';
+
+  images = [
     {
-      id: 0,
-      name: 'Star Wars',
-      description: 'Once Upon a Time',
-      photo: `${this.baseUrl}/example-house.jpg`,
+      imgSrc: 'https://static.vecteezy.com/system/resources/thumbnails/003/582/701/small/coming-soon-background-illustration-template-design-free-vector.jpg',
+      imgAlt: 'Image 1',
     },
     {
-      id: 1,
-      name: 'Clone Wars',
-      description: 'Once Upon a Time',
-      photo: `${this.baseUrl}/example-house.jpg`,
+      imgSrc: 'https://static.vecteezy.com/system/resources/thumbnails/003/582/701/small/coming-soon-background-illustration-template-design-free-vector.jpg',
+      imgAlt: 'Image 2',
     },
-  ];
+  ]
 }
